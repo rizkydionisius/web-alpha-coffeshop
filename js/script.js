1c5fa8756de48ec497027e20cb54f4efdf1436c5
rizkydionisius <dionpramu86@gmail.com>
2024-03-22 08:06:48 +0700

    menambahkan section produk kami, menjalankan fitur keranjang, dan menambahkan detail produk. Terdapat bug yaitu ketika icon detail di klik tidak mengarah ke modal box namun mengarah ke home

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-//Toggle class active
+//Toggle class active untuk Hamburger Menu
 const navbarNav=document.querySelector('.navbar-nav');
 
 //Ketika hamburger menu di klik 
@@ -6,10 +6,46 @@ document.querySelector('#hamburger-menu').onclick = () =>{
     navbarNav.classList.toggle('active');
 }
 
-// Click di luar sidebar untuk menghilangkan nav
-const hamburger= document.querySelector('#hamburger-menu');
+// Toggle Class Active untuk Search Form
+const searchForm = document.querySelector('.search-form');
+const searchBox = document.querySelector('#search-box');
+
+document.querySelector  ('#search-button').onclick = ()=>{
+    searchForm.classList.toggle('active');
+    searchBox.focus();
+    e.preventDefault();
+};
+
+// Toggle Class Active untuk Shopping Cart
+const shoppingCart = document.querySelector('.shopping-cart');
+document.querySelector('#shopping-cart-button').onclick=()=>{
+    shoppingCart.classList.toggle('active');
+    e.preventDefault
+};
+
+// Click di luar elemen
+const hm= document.querySelector('#hamburger-menu');
+const sb= document.querySelector('#search-button');
+const sc= document.querySelector('#shopping-cart-button');
+
 document.addEventListener('click', function(e){
-    if(!hamburger.contains(e.target) && !navbarNav.contains(e.target)){
+    if(!hm.contains(e.target) && !navbarNav.contains(e.target)){
         navbarNav.classList.remove('active');
     }
+    if(!sb.contains(e.target) && !searchForm.contains(e.target)){
+        searchForm.classList.remove('active');
+    }
+    if(!sc.contains(e.target) && !shoppingCart.contains(e.target)){
+        shoppingCart.classList.remove('active');
+    }
 })
+
+
+//Modal Box
+const itemDetailModal = document.querySelector('#item-detail-modal');
+const itemDetailButton = document.querySelector('.item-detail.button');
+
+itemDetailButton.onclick = (e) => {
+    itemDetailModal.style.display ='flex';
+    e.preventDefault();
+};
